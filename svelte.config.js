8add3948cb8a00f82237ad96be3afe8fcf20039e
Alex Booster <webdevbooster@gmail.com>
2025-08-31 12:30:49 +0100

    Prep for single self-contained HTML app

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
-};
-
+		adapter: adapter(),
+		router: { type: 'hash' }, // hash-based routing (disables SSR/prerender)
+		output: { bundleStrategy: 'inline' } // single-file: JS+CSS inlined into HTML
+		}
+	};
 export default config;
